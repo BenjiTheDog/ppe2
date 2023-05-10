2501b0d2d700c642d1d110b10c1b97198ede7c4a
Xx-Remi-xX <Xx-Remi-xX@users.noreply.github.com>
2023-05-10 11:06:06 +0200

    efef

--- inscription.php
+++ inscription.php
@@ -97,21 +97,15 @@ input[type=submit]:hover {
  <form action="verification.php" method="POST">
  <h1>Inscription</h1>
  
- <label><b>Nom d'utilisateur</b></label>
- <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
- <label><b>Mot de passe</b></label>
- <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
- <input type="submit" id='submit' value="S'inscrire" >
+ <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<label for="username">Nom d'utilisateur :</label>
+		<input type="text" name="username" id="username" required>
+		<label for="password">Mot de passe :</label>
+		<input type="password" name="password" id="password" required>
+		<input type="submit" name="submit" value="S'inscrire">
+	</form>
  
- <?php
- if(isset($_GET['erreur'])){
- $err = $_GET['erreur'];
- if($err==1 || $err==2)
- echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
- }
- ?>
+ 
  
  </form>
  </div>
