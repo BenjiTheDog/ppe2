d39d22e32c588aab51cdc7d8e3482d90368a49b0
Xx-Remi-xX <Xx-Remi-xX@users.noreply.github.com>
2023-05-10 11:07:09 +0200

    rere

--- inscription.php
+++ inscription.php
@@ -1,8 +1,8 @@
 <?php
 // Connexion à la base de données MySQL
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "nom_utilisateur";
+$password = "mot_de_passe";
 $dbname = "utilisateur";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
@@ -18,13 +18,13 @@ if (isset($_POST['submit'])) {
 	$password = $_POST['password'];
 
 	// Vérification si le nom d'utilisateur existe déjà dans la base de données
-	$sql = "SELECT * FROM utilisateur WHERE nom_utilisateur='$username'";
+	$sql = "SELECT * FROM utilisateur WHERE username='$username'";
 	$result = mysqli_query($conn, $sql);
 	if (mysqli_num_rows($result) > 0) {
 		echo "Le nom d'utilisateur existe déjà.";
 	} else {
 		// Insertion des données dans la base de données
-		$sql = "INSERT INTO utilisateur (nom_utilisateur, mot_de_passe) VALUES ('$username', '$password')";
+		$sql = "INSERT INTO utilisateur (nom_utilisateur,mot_de_passe) VALUES ('$username', '$password')";
 		if (mysqli_query($conn, $sql)) {
 			echo "Inscription réussie.";
 		} else {
@@ -36,79 +36,20 @@ if (isset($_POST['submit'])) {
 // Fermeture de la connexion à la base de données
 mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
 <html>
- <head>
- <meta charset="utf-8">
-<style>
-body{
- background: #67BE4B;
-}
-#container{
- width:400px;
- margin:0 auto;
- margin-top:10%;
-}
-/* Bordered form */
-form {
- width:100%;
- padding: 30px;
- border: 1px solid #f1f1f1;
- background: #fff;
- box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-}
-#container h1{
- width: 38%;
- margin: 0 auto;
- padding-bottom: 10px;
-}
-
-/* Full-width inputs */
-input[type=text], input[type=password] {
- width: 100%;
- padding: 12px 20px;
- margin: 8px 0;
- display: inline-block;
- border: 1px solid #ccc;
- box-sizing: border-box;
-}
-
-/* Set a style for all buttons */
-input[type=submit] {
- background-color: #53af57;
- color: white;
- padding: 14px 20px;
- margin: 8px 0;
- border: none;
- cursor: pointer;
- width: 100%;
-}
-input[type=submit]:hover {
- background-color: white;
- color: #53af57;
- border: 1px solid #53af57;
-}
-
-</style>
- </head>
- <body>
- <div id="container">
- <!-- zone de connexion -->
- 
- <form action="verification.php" method="POST">
- <h1>Inscription</h1>
- 
- <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<head>
+	<title>Inscription</title>
+</head>
+<body>
+	<h2>Inscription</h2>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 		<label for="username">Nom d'utilisateur :</label>
 		<input type="text" name="username" id="username" required>
 		<label for="password">Mot de passe :</label>
 		<input type="password" name="password" id="password" required>
 		<input type="submit" name="submit" value="S'inscrire">
 	</form>
- 
- 
- 
- </form>
- </div>
-
- </body>
+</body>
 </html>
