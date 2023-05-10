4cfbc6e5e576d525a04662a94838f42bd4f58f4d
Xx-Remi-xX <Xx-Remi-xX@users.noreply.github.com>
2023-05-10 10:56:38 +0200

    reer

--- inscription.php
+++ inscription.php
@@ -1,3 +1,41 @@
+<?php
+// Connexion à la base de données MySQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "utilisateur";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Vérification de la connexion
+if (!$conn) {
+	die("Connexion échouée : " . mysqli_connect_error());
+}
+
+// Vérification si le formulaire a été soumis
+if (isset($_POST['submit'])) {
+	// Récupération des données du formulaire
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	// Vérification si le nom d'utilisateur existe déjà dans la base de données
+	$sql = "SELECT * FROM utilisateur WHERE nom_utilisateur='$username'";
+	$result = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+		echo "Le nom d'utilisateur existe déjà.";
+	} else {
+		// Insertion des données dans la base de données
+		$sql = "INSERT INTO utilisateur (nom_utilisateur, mot_de_passe) VALUES ('$username', '$password')";
+		if (mysqli_query($conn, $sql)) {
+			echo "Inscription réussie.";
+		} else {
+			echo "Erreur : " . mysqli_error($conn);
+		}
+	}
+}
+
+// Fermeture de la connexion à la base de données
+mysqli_close($conn);
+?>
 <html>
  <head>
  <meta charset="utf-8">
