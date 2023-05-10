9397d3fa1f2f341b4d0a54e93b6c8aefba3215dd
Xx-Remi-xX <Xx-Remi-xX@users.noreply.github.com>
2023-05-10 09:44:08 +0200

    rere

--- login.php
+++ login.php
@@ -74,28 +74,6 @@ input[type=submit]:hover {
  ?>
  </form>
  </div>
- <div id="content">
- 
- <a href='principale.php?deconnexion=true'><span>Déconnexion</span></a>
- 
- <!-- tester si l'utilisateur est connecté -->
- <?php
- session_start();
- if(isset($_GET['deconnexion']))
- { 
- if($_GET['deconnexion']==true)
- { 
- session_unset();
- header("location:login.php");
- }
- }
- else if($_SESSION['username'] !== ""){
- $user = $_SESSION['username'];
- // afficher un message
- echo "<br>Bonjour $user, vous êtes connectés";
- }
- ?>
- 
- </div>
+
  </body>
 </html>
